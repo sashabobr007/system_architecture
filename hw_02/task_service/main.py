e83b1a867396fc9caafb332a9cb80183c003f501
aleksandralekseev <alekseev.job.official@mail.ru>
2025-03-24 14:50:28 +0300

    added userservice

--- hw_02/task_service/main.py
+++ hw_02/task_service/main.py
@@ -123,6 +123,10 @@ def delete_user(user_id: int, current_user: str = Depends(get_current_client)):
             return deleted_user
     raise HTTPException(status_code=404, detail="User not found")
 
+@app.get("/")
+async def read_root():
+    return {"User Service": "Running"}
+
 
 if __name__ == "__main__":
     import uvicorn
